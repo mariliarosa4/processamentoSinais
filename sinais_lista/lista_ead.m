50ec65991a93df8d04a9cb8ed6f143bd067d16dd
mariliarosa4 <mariliasi@ufcspa.edu.br>
2018-04-25 18:12:28 -0300

    teste lista

--- sinais_lista/lista_ead.m
+++ sinais_lista/lista_ead.m
@@ -1,13 +1,13 @@
 clear all;
 ecgAmostra = load('ecg.txt'); 
 pkg load signal;
-sinal = ecgAmostra(1:15000);
-
+sinal = ecgAmostra(1:10000);
+x = 1:1:10000;
 data2 = (sinal); 
-[pks2 idx2] = findpeaks(data2,"DoubleSided", 'MinPeakDistance', 20);
+[pks2 idx2] = findpeaks(data2,"DoubleSided", 'MinPeakDistance', 100);
 figure
-plot(sinal)
-figure
- plot(sinal,data2,sinal(idx2),data2(idx2),".m")
+ plot(x,data2,x(idx2),data2(idx2),"or")
 
-
+ for k:100:size(data2)
+   
+   
