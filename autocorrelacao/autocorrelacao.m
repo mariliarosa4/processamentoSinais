29dfe0acce75be70ebfccaf354baa443aee6e3c4
mariliarosa4 <mariliasi@ufcspa.edu.br>
2018-05-18 17:42:45 -0300

    versao com plot

--- autocorrelacao/autocorrelacao.m
+++ autocorrelacao/autocorrelacao.m
@@ -4,24 +4,37 @@ clear all;
 ecgAmostra = load('ecg.txt'); 
 pkg load signal;
 
-janela = 1; #trocar pra 1000
-num_janelas = 10;
+janela = 1000; #trocar pra 1000
+num_janelas = 1;
+total = janela * num_janelas;
 
 sinal = ecgAmostra(1:(janela*num_janelas)); #pegando uma parte do sinal de 10 mil amostras
 
 pronta = xcorr(sinal);
 somatorio = zeros(size(sinal));
-corr = zeros(size(sinal));
+autocorr = zeros(size(sinal));
 
-for (i=1:10)
-  cont=0;
-  for(j=1:10)
+for (i=1:total)
+  for(j=1:total)
     multiplica=0;
-    if (((j-1)+i)<=10)
+    if (((j-1)+i)<=total)
     multiplica = sinal(j) * sinal((j-1)+i);
     endif
-    cont = cont+1;  
     somatorio(i)= multiplica + somatorio(i);
     endfor
-  corr(i) = somatorio(i);
+  autocorr(i) = somatorio(i);
   endfor
+
+figure
+subplot(1,2,1);
+stem(pronta)
+title('Função xcorr');
+
+
+
+corr_flip = fliplr(autocorr);
+autocorr_final = [corr_flip, autocorr];
+
+subplot(1,2,2);
+stem(autocorr_final)
+title("Função própria");
