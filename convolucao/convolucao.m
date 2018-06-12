47b8e695d839a7d5827d06af678fc5a21c0ca895
mariliarosa4 <mariliasi@ufcspa.edu.br>
2018-06-12 17:40:14 -0300

    Convolucao

--- convolucao/convolucao.m
+++ convolucao/convolucao.m
@@ -8,19 +8,21 @@ yRot = flip(y);
 
 #calcular g(t)
 
-conv = zeros(size(x));
-somatorio = zeros(1,size(x,2));
+convo = zeros(size(x));
+somatorio = zeros(1,size(x,2)+2);
 
 
-for (i=1:size(y,2))
-  for(j=1:size(x,2))
+for (i=1:size(x,2)+2)
+  for(j=size(yRot,2):-1:1)
     multiplica=0;
-    if (j+(i-1)<=size(yRot,2))
-    multiplica = x(j + (i-1)) * yRot(j)
+    if (j+(i-size(yRot,2)) <=size(x,2) && j+(i-3)>=1)
+    multiplica = x(j+(i-3)) * yRot(j)
     endif
     somatorio(i)= multiplica + somatorio(i)
     endfor
-  conv(i) = somatorio(i);
+  convo(i) = somatorio(i);
   endfor
   
-  stem(conv)
+  stem(convo)
+  
+  verdadeira = conv(x,y)
